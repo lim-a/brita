ef384b223af30d2db3b3b33cdae66f19a2f3653d
lim-a <piopiopio1992@gmail.com>
2020-12-30 11:22:04 +0900

    Upload files

---
+++ js/custom.js
@@ -0,0 +1,144 @@
+﻿$(document).ready(function( ) {
+	var ht = $(window).height( );
+	$('section').height(ht);
+    
+	$(window).on("resize", function( ) {
+		var ht = $(window).height( );
+		$('section').height(ht);
+	});
+    
+	$('#menu li').on("click", function(e) {
+		e.preventDefault( );
+		var ht = $(window).height( );
+		var i = $(this).index( );
+		var scPagePos = ht * i;
+		
+		$('html, body').stop( ).animate({"scrollTop":scPagePos}, 1400);
+	}); 
+    
+	$(window).on("scroll", function( ) {
+		var ht = $(window).height( );
+		var scroll = $(window).scrollTop( );
+		
+		for(var i=0; i<5; i++) {
+			if(scroll>=ht*i-50 && scroll<ht*(i+1)-50) {
+				$('#menu li').removeClass( );
+				$('#menu li').eq(i).addClass("on");
+			}
+		}		
+	}); 
+	
+	$('section').on("mousewheel", function(event, delta) {
+		if(delta > 0) { 
+			var prev = $(this).prev( ).offset( ).top;
+			
+			$('html, body').stop( ).animate({"scrollTop":prev}, 1400, "easeOutBounce");
+		}else if(delta < 0) {
+			var next = $(this).next( ).offset( ).top;
+			$('html, body').stop( ).animate({"scrollTop":next}, 1400, "easeOutBounce");
+		}
+	});
+    
+});
+
+$(document).ready(function(){
+
+    $('.sec2-wrap article').on("mouseover", function(){
+        var vid = $(this).find("video").get(0);
+            vid.currentTime = 0; 
+            vid.play(); 
+        
+        $(this).stop().animate({"width":"28%"}, 800, function(){
+            $(this).find('h3').stop().animate({"right":"250px"}, 400);
+            $(this).find('p').stop().animate({"left":"50px"}, 700);
+        });
+        $(this).find('video').stop().animate({"opacity":"1"}, 1200);
+    });
+    
+    $('.sec2-wrap article').on("mouseout", function(){
+        var vid = $(this).find("video").get(0);
+        vid.pause();
+        
+        $(this).stop().animate({"width":"24%"}, 700);
+        $(this).find('video').stop().animate({"opacity":"0"}, 1000);
+        $(this).find('h3').stop().animate({"right":"-350px"}, 200);
+        $(this).find('p').stop().animate({"left":"-350px"}, 500);
+    });
+    
+    function active(ci,sc,ht){
+		var wsc = $(this).scrollTop();
+		if(wsc > $(ci).offset().top - ($(this).height()/ht)){
+			$(ci).find(sc).addClass('active');
+		}else{
+			$(ci).find(sc).removeClass('active');
+		}
+	};
+    
+    $(window).scroll(function () {
+        active('.sec3-wrap', '.sec3-text', '2');
+        active('.square-wrap', '.square', '1');
+        active('.square', '.sec3-txt','3');
+        active('.sec4-wrap', '.img', '2');
+        active('.sec4-wrap', '.txt', '3');
+        active('.sec4-wrap', '.search', '7');
+        active('.square', '.sec3-txt','2');
+    });
+    
+    TweenMax.to(".wrapper", 2, {
+        top: "-100%",
+        ease: Expo.easeInOut,
+        delay: 2.5
+    });
+
+    var tl = new TimelineMax();
+
+    tl.from(".loader", 1, {
+        scaleY: "0%",
+        y: 80,
+        ease: Expo.easeInOut,
+        delay: 0.5,
+        transformOrigin: "50% 100%"
+    });
+
+    tl.to(".loader", 1.6, {
+        height: "20vh",
+        scaleY: "0%",
+        ease: Expo.easeInOut,
+        transformOrigin: "0% -100%"
+    });
+});
+
+	$(function(){
+		size();
+		$(window).resize(function(){
+			size();
+		})
+		function size(){
+			var wid = $(window).innerWidth()+17;
+			var hei = $(window).innerHeight();
+			$(".mfull-wi").css("width",wid);
+			$(".mfull-wi").css("height",hei);
+		}
+	});
+
+$(document).ready(function () {
+    var btn = $(".menu-btn");
+    btn.click(function () {
+        var chk = $(".mfull").width();
+        if (chk == 0) {
+            $("#wrap").addClass("btn_on");
+        } else {
+            $("#wrap").removeClass("btn_on");
+        }
+    });
+    $(".mfull-ul li a").click(function () {
+        $("#wrap").removeClass("btn_on");
+    });
+});
+
+$(function(){
+    setTimeout(function(){
+        $('.maintext').removeClass('hidden');
+        $('.maintext2').removeClass('hidden');
+    }, 800);
+});
---
+++ js/pure.js
@@ -0,0 +1,149 @@
+ /**
+        * Performs the movement (by CSS3 or by jQuery)
+        */
+        function performMovement(v){
+            // using CSS3 translate functionality
+            if (options.css3 && options.autoScrolling && !options.scrollBar) {
+                var translate3d = 'translate3d(0px, -' + v.dtop + 'px, 0px)';
+                transformContainer(translate3d, true);
+
+                //even when the scrollingSpeed is 0 there's a little delay, which might cause the
+                //scrollingSpeed to change in case of using silentMoveTo();
+                if(options.scrollingSpeed){
+                    afterSectionLoadsId = setTimeout(function () {
+                        afterSectionLoads(v);
+                    }, options.scrollingSpeed);
+                }else{
+                    afterSectionLoads(v);
+                }
+            }
+
+            // using jQuery animate
+            else{
+                var scrollSettings = getScrollSettings(v);
+
+                $(scrollSettings.element).animate(
+                    scrollSettings.options,
+                options.scrollingSpeed, options.easing).promise().done(function () { //only one single callback in case of animating  `html, body`
+                    if(options.scrollBar){
+
+                        /* Hack!
+                        The timeout prevents setting the most dominant section in the viewport as "active" when the user
+                        scrolled to a smaller section by using the mousewheel (auto scrolling) rather than draging the scroll bar.
+
+                        When using scrollBar:true It seems like the scroll events still getting propagated even after the scrolling animation has finished.
+                        */
+                        setTimeout(function(){
+                            afterSectionLoads(v);
+                        },30);
+                    }else{
+                        afterSectionLoads(v);
+                    }
+                });
+            }
+        }
+
+        /**
+        * Gets the scrolling settings depending on the plugin autoScrolling option
+        */
+        function getScrollSettings(v){
+            var scroll = {};
+
+            if(options.autoScrolling && !options.scrollBar){
+                scroll.options = { 'top': -v.dtop};
+                scroll.element = WRAPPER_SEL;
+            }else{
+                scroll.options = { 'scrollTop': v.dtop};
+                scroll.element = 'html, body';
+            }
+
+            return scroll;
+        }
+
+        /**
+        * Adds sections before or after the current one to create the infinite effect.
+        */
+        function createInfiniteSections(v){
+            // Scrolling down
+            if (!v.isMovementUp) {
+                // Move all previous sections to after the active section
+                $(SECTION_ACTIVE_SEL).after(v.activeSection.prevAll(SECTION_SEL).get().reverse());
+            }
+            else { // Scrolling up
+                // Move all next sections to before the active section
+                $(SECTION_ACTIVE_SEL).before(v.activeSection.nextAll(SECTION_SEL));
+            }
+
+            // Maintain the displayed position (now that we changed the element order)
+            silentScroll($(SECTION_ACTIVE_SEL).position().top);
+
+            // Maintain the active slides visible in the viewport
+            keepSlidesPosition();
+
+            // save for later the elements that still need to be reordered
+            v.wrapAroundElements = v.activeSection;
+
+            // Recalculate animation variables
+            v.dtop = v.element.position().top;
+            v.yMovement = getYmovement(v.element);
+
+            return v;
+        }
+
+        /**
+        * Fix section order after continuousVertical changes have been animated
+        */
+        function continuousVerticalFixSectionOrder (v) {
+            // If continuousVertical is in effect (and autoScrolling would also be in effect then),
+            // finish moving the elements around so the direct navigation will function more simply
+            if (!v.wrapAroundElements || !v.wrapAroundElements.length) {
+                return;
+            }
+
+            if (v.isMovementUp) {
+                $(SECTION_FIRST_SEL).before(v.wrapAroundElements);
+            }
+            else {
+                $(SECTION_LAST_SEL).after(v.wrapAroundElements);
+            }
+
+            silentScroll($(SECTION_ACTIVE_SEL).position().top);
+
+            // Maintain the active slides visible in the viewport
+            keepSlidesPosition();
+        }
+
+
+        /**
+        * Actions to do once the section is loaded.
+        */
+        function afterSectionLoads (v){
+            continuousVerticalFixSectionOrder(v);
+
+            //callback (afterLoad) if the site is not just resizing and readjusting the slides
+            $.isFunction(options.afterLoad) && !v.localIsResizing && options.afterLoad.call(v.element, v.anchorLink, (v.sectionIndex + 1));
+            options.scrollOverflowHandler.afterLoad();
+
+            playMedia(v.element);
+            v.element.addClass(COMPLETELY).siblings().removeClass(COMPLETELY);
+
+            canScroll = true;
+
+            $.isFunction(v.callback) && v.callback.call(this);
+        }
+
+        /**
+        * Lazy loads image, video and audio elements.
+        */
+        function lazyLoad(destiny){
+            var destiny = getSlideOrSection(destiny);
+
+            destiny.find('img[data-src], source[data-src], audio[data-src], iframe[data-src]').each(function(){
+                $(this).attr('src', $(this).data('src'));
+                $(this).removeAttr('data-src');
+
+                if($(this).is('source')){
+                    $(this).closest('video').get(0).load();
+                }
+            });
+        }
